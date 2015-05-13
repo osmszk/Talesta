fc2f2cf31ea3896f383ba4effc26591abcad9cef
osuzukix <osuzuki@plegineer.com>
2015-05-13 21:23:08 +0900

    Initial Commit

---
+++ InstaSearch/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  InstaSearch
+//
+//  Created by 鈴木治 on 2015/05/13.
+//  Copyright (c) 2015年 Plegineer Inc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

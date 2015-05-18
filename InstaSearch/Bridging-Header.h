1d5ce3e28ba404a7d487beb289d026fd072e8e4c
osuzukix <osuzuki@plegineer.com>
2015-05-18 22:35:49 +0900

    AFNetworkingライブラリ追加

---
+++ InstaSearch/Bridging-Header.h
@@ -0,0 +1,21 @@
+//
+//  Bridging-Header.h
+//  InstaSearch
+//
+//  Created by 鈴木治 on 2015/05/18.
+//  Copyright (c) 2015年 Plegineer Inc. All rights reserved.
+//
+
+#ifndef InstaSearch_Bridging_Header_h
+#define InstaSearch_Bridging_Header_h
+
+
+#ifndef AFNetworkSample_Bridging_Header_h
+#define AFNetworkSample_Bridging_Header_h
+
+#import "AFNetworking.h"
+//#import "AFNetworkActivityLogger.h"
+
+#endif
+
+#endif

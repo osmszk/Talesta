b6f9a83e19b6c097d04008a85f61f671f00cffe6
osuzukix <osuzuki@plegineer.com>
2015-05-31 14:35:41 +0900

    コメント追加

--- InstaSearch/FollowerRankingViewController.swift
+++ InstaSearch/FollowerRankingViewController.swift
@@ -48,6 +48,8 @@ class FollowerRankingViewController: UIViewController {
             parameters: nil,
             timeoutInterval: 10,
             success: { (operation : AFHTTPRequestOperation!, responsobject: AnyObject!) -> Void in
+                //クロージャ
+                //ref:http://ja.wikipedia.org/wiki/Swift_%28%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0%E8%A8%80%E8%AA%9E%29#.E3.82.AF.E3.83.AD.E3.83.BC.E3.82.B8.E3.83.A3
                 
                 SVProgressHUD.dismiss()
                 

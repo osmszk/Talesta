ed9e4421503b5ea083803af4b37dcfe5deaf2f09
osuzukix <osuzuki@plegineer.com>
2015-07-07 09:45:08 +0900

    タレントモデルクラス追加

---
+++ InstaSearch/TalentModel.swift
@@ -0,0 +1,18 @@
+//
+//  TalentModel.swift
+//  InstaSearch
+//
+//  Created by 鈴木治 on 2015/07/07.
+//  Copyright (c) 2015年 Plegineer Inc. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class TalentModel: Object {
+    dynamic var id : String = ""
+    dynamic var name : String = ""
+    dynamic var url : String = ""
+    
+    
+}

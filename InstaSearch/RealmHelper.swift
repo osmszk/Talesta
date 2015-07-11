7cb801648b3c4fbe22f4e690ef52e37e18adda63
osuzukix <osuzuki@plegineer.com>
2015-07-11 21:54:56 +0900

    ひらがなとカタカナも考慮

--- InstaSearch/RealmHelper.swift
+++ InstaSearch/RealmHelper.swift
@@ -77,7 +77,7 @@ class RealmHelper: NSObject {
     
     
     class func talentSectionTitles() -> [String]{
-        return ["0","A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q" ,"R", "S", "T", "U", "V", "W", "X", "Y", "Z","#"]
+        return ["0","A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q" ,"R", "S", "T", "U", "V", "W", "X", "Y", "Z","あ","ア","漢","#"]
     }
     
     class func cellDataIndexTalents(talentModels:Results<TalentModel>) -> [IndexTitleTalent]{
@@ -94,7 +94,7 @@ class RealmHelper: NSObject {
                 let name = talentModel.name
                 let nameHead = (name as NSString).substringToIndex(1).uppercaseString
                 
-                if shouldAddGroup(nameHead, indexTitle: indexTitle){
+                if self.shouldAddGroup(nameHead, indexTitle: indexTitle){
                     talentsGroup.append(talentModel)
                 }
             }
@@ -105,20 +105,55 @@ class RealmHelper: NSObject {
     }
     
     class func shouldAddGroup(nameHead:String ,indexTitle:String)->Bool {
+        //http://qiita.com/hiroo0529/items/c49416c7587daa61ff90
         if indexTitle == "0"{
             return self.isDigit(nameHead)
         }else if indexTitle >= "A" && indexTitle <= "Z"{
             return nameHead == indexTitle
+        }else if indexTitle == "あ"{
+            return self.isIncludeHiragana(nameHead)
+        }else if indexTitle == "ア"{
+            return self.isIncludeKatakana(nameHead)
+        }else  if indexTitle == "漢"{
+            return !self.isDigit(nameHead)
+                && !self.isAlphabet(nameHead)
+                && !self.isIncludeHiragana(nameHead)
+                && !self.isIncludeKatakana(nameHead)
+                && nameHead != "["
         }else if indexTitle == "#"{
-            return !self.isDigit(nameHead) && !(nameHead >= "A" && nameHead <= "Z")
+            return nameHead == "["
+        }
+        return false
+    }
+    
+    //http://qiita.com/yasumodev/items/4f1e859da4986aaca55e
+    class func isAlphabet(string:String) -> Bool {
+        return string >= "A" && string <= "Z"
+    }
+    
+    //http://qiita.com/MIN/items/a37b6e1ce0f41872fd2c
+    class func isIncludeKatakana(string:String) -> Bool{
+        for c in string.unicodeScalars {
+            if c.value >= 0x30A1 && c.value <= 0x30F6{
+                return true
+            }
+        }
+        return false
+    }
+    
+    class func isIncludeHiragana(string:String) -> Bool{
+        for c in string.unicodeScalars {
+            if c.value >= 0x3041 && c.value <= 0x3096{
+                return true
+            }
         }
         return false
     }
 
-    class func isDigit(string:String) ->Bool{
+    class func isDigit(string:String) -> Bool{
 
-        for tempChar in string.unicodeScalars {
-            let s = String(tempChar).unicodeScalars
+        for c in string.unicodeScalars {
+            let s = String(c).unicodeScalars
             let uni = s[s.startIndex]
             
             let digits = NSCharacterSet.decimalDigitCharacterSet()
